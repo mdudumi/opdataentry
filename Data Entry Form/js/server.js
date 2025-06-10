3eed632d44e726c27aa401cd94a2e63bc724edc3
mdudumi <martin.dudumi@gmail.com>
2025-06-10 15:18:43 +0200

    Update server.js

--- Data Entry Form/js/server.js
+++ Data Entry Form/js/server.js
@@ -8,11 +8,11 @@ const port = 3000;
 
 // PostgreSQL connection pool — adjust your env vars as needed
 const pool = new Pool({
-  user: process.env.PGUSER,
-  host: process.env.PGHOST,
-  database: process.env.PGDATABASE,
-  password: process.env.PGPASSWORD,
-  port: process.env.PGPORT,
+    user: 'postgres',
+    host: '34.32.41.224',
+    database: 'OPDES',
+    password: 'South123',
+    port: 5432,
 });
 
 // Middleware
